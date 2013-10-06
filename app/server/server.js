8d0366f67af01d426f9f79282bd6fc580ea13338
mgarbacz <mich.garbacz@gmail.com>
2013-10-06 16:29:31 -0500

    Wrote basic chat server
    To experiment with socket.io, wrote a quick and dirty chat server.

--- app/server/server.js
+++ app/server/server.js
@@ -1,8 +1,15 @@
 var io = require('socket.io').listen(8888);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { news: 'Hello world!' });
+
   socket.on('message', function (data) {
     console.log(data);
+    io.sockets.emit('message', { message: data.message });
   });
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('message', { message: 'A user has disconnected!'});
+  });
+
 });
+
