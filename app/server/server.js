bc51091fc8dd750d309bf997bb073736a87ac679
mgarbacz <mich.garbacz@gmail.com>
2013-10-06 18:48:03 -0500

    Moved chat functionality to separate context
    Moved chat frontend into chat.html and put chat socket under its own
    context.

--- app/server/server.js
+++ app/server/server.js
@@ -1,14 +1,13 @@
 var io = require('socket.io').listen(8888);
 
-io.sockets.on('connection', function (socket) {
+var chat = io.of('/chat').on('connection', function(socket) {
 
-  socket.on('message', function (data) {
-    console.log(data);
-    io.sockets.emit('message', { message: data.message });
+  socket.on('message', function() {
+    chat.emit('message', { message: data.message });
   });
 
-  socket.on('disconnect', function () {
-    io.sockets.emit('message', { message: 'A user has disconnected!'});
+  socket.on('disconnect', function() {
+    chat.emit('message', { message: 'A user has disconnected!'});
   });
 
 });
