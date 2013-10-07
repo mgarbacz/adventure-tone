bb8ff9b188c384c4e512dfdaa89eb167f3df0795
mgarbacz <mich.garbacz@gmail.com>
2013-10-07 13:55:40 -0500

    Add username setting/getting on server
    Add dirty functionality to store username on server. Will cleanup.

--- app/server/server.js
+++ app/server/server.js
@@ -6,16 +6,50 @@ var tones = io.of('/tones').on('connection', function(socket) {
     console.log(data);
     socket.emit('greeting', { greeting: 'Hello client!'})
   });
+
 });
 
 var chat = io.of('/chat').on('connection', function(socket) {
 
+  // Set the default usename to socket's id
+  socket.set('username', socket.id);
+
+  // Set the username on request from client and announce connection
+  socket.on('set username', function(username) {
+    if (username != '') {
+      socket.set('username', username, function() {
+        socket.emit('username ready');
+      });
+    }
+    socket.get('username', function(err, name) {
+      if (!err)
+        chat.emit('message', { message: name + ' has connected'});
+    });
+  });
+
+  // Change the username on request from client and announce change
+  socket.on('change username', function(username) {
+    if (username != '') {
+      socket.get('username', function(err, old_name) {
+        socket.set('username', username, function() {
+          socket.emit('username ready');
+          chat.emit('message', { message: old_name + ' is now ' + username});
+        });
+      });
+    }
+  });
+
+  // Announce messages sent by the clients
   socket.on('message', function(data) {
     chat.emit('message', { message: data.message });
   });
 
+  // Announce disconnection
   socket.on('disconnect', function() {
-    chat.emit('message', { message: 'A user has disconnected!'});
+    socket.get('username', function(err, username) {
+      if (!err)
+        chat.emit('message', { message: username + ' has disconnected'});
+    });
   });
 
 });
